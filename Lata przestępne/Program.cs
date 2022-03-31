150bbb9e2c8860ac16d32a388e187d1d6ee30d1b
Alicja <alichr07@gmail.com>
2022-03-31 20:51:01 +0200

    Dodaj pliki projektów.

---
+++ Lata przestępne/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lata_przestępne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sprawdzanie roku przestępnego
+
+            Console.WriteLine("Podaj rok:");
+            int rok = int.Parse(Console.ReadLine());
+
+            if (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0)
+            {
+                Console.WriteLine("To jest rok przestępny.");
+            }
+            else
+            {
+                Console.WriteLine("To nie jest rok przestępny.");
+            }
+        }
+    }
+}
